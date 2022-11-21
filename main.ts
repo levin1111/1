15cf6ad0238c732ba31ef9aefb6940762db30be0
levin1111 <118731910+levin1111@users.noreply.github.com>
2022-11-21 11:55:18 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,28 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+radio.setGroup(123)
+basic.forever(function () {
+    let _1 = 0
+    if (_1 == 1) {
+        basic.showIcon(IconNames.Happy)
+    }
+    if (_1 == 2) {
+        basic.showIcon(IconNames.Sad)
+    }
+    if (_1 == 3) {
+        basic.showIcon(IconNames.Confused)
+    }
+    if (_1 == 4) {
+        basic.showIcon(IconNames.Angry)
+    }
+    if (_1 == 5) {
+        basic.showIcon(IconNames.Asleep)
+    }
+    if (_1 == 6) {
+        basic.showIcon(IconNames.Surprised)
+    }
+})
